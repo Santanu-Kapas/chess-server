7516a302720f74dd296368bf4da74fd05a242981
Santanu-Kapas <142741328+Santanu-Kapas@users.noreply.github.com>
2024-04-21 14:52:19 +0000

    db connection string update

--- database.js
+++ database.js
@@ -2,11 +2,7 @@ const pg =require("pg");
 require('dotenv').config();
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString:process.env.DB_connecionString,
 });
 console.log("db connects sucessfully!");
 module.exports=db;
