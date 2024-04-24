4d43388b694564fbfe7de7bf0fdf0a907ba45e45
Puspendu Bera <109166740+PuspenduBera2003@users.noreply.github.com>
2024-04-24 18:22:51 +0000

    some minor changes

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(passport.session());
 const server = createServer(app);
 const io = initializeSocket(server);
 
-app.use(express.static(path.join(__dirname, 'chess-client/build')));
+app.use(express.static(path.join(__dirname, './chess-client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './chess-client/build', 'index.html'));
